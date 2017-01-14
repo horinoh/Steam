e3ef54551308a6dab202d4c13eb50f0782af2234
horinoh <development@horino.sakura.ne.jp>
2017-01-14 15:25:40 +0900

    バグ修正 appid.txt追加

--- Steam/STEAMConfig.cmake
+++ Steam/STEAMConfig.cmake
@@ -1,5 +1,5 @@
-set(VULKAN_INCLUDE_DIRS $ENV{STEAM_SDK_PATH}/public)
-set(VULKAN_LIBRARY_DIRS $ENV{STEAM_SDK_PATH}/redistributable_bin/win64)
-set(VULKAN_LIBRARIES debug steam_api64 optimized steam_api64)
+set(STEAM_INCLUDE_DIRS $ENV{STEAM_SDK_PATH}/public)
+set(STEAM_LIBRARY_DIRS $ENV{STEAM_SDK_PATH}/redistributable_bin/win64)
+set(STEAM_LIBRARIES debug steam_api64 optimized steam_api64)
 
 mark_as_advanced(STEAM)
--- Steam/Steam.h
+++ Steam/Steam.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "resource.h"
+
+#pragma warning(disable:4819)
+#pragma warning(disable:4996)
+#include "steam/steam_api.h"
