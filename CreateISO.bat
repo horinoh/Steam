d579e87275d8ea0ed50e2ed96d40c4be91580380
horinoh <development@horino.sakura.ne.jp>
2018-07-08 14:44:27 +0900

    バッチ変更

--- CreateISO.bat
+++ CreateISO.bat
@@ -1,12 +1,9 @@
 @echo off
 
 rem https://cdrtfe.sourceforge.io/cdrtfe/index_en.html インストールしておく
+rem %CDRTOOLS%\cygwin を環境変数 Path に通しておく
 set CDRTOOLS="C:\Program Files (x86)\cdrtfe\tools"
-
-xcopy %CDRTOOLS%\cygwin\cygwin1.dll . /Y
 
 for /d %%i in (GoldMasterDisk/*) do (
     %CDRTOOLS%\cdrtools\mkisofs -r -J -o GoldMasterDisk/%%i.iso GoldMasterDisk/%%i
 )
-
-del cygwin1.dll
